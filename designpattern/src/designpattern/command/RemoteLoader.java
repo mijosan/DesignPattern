ebb0cb6b956cdab817c6cdda84fe84246c206e41
ChoiTaesan <mijosan@naver.com>
2020-08-10 16:22:19 +0900

    Command Pattern

--- designpattern/src/designpattern/command/RemoteLoader.java
+++ designpattern/src/designpattern/command/RemoteLoader.java
@@ -4,24 +4,21 @@ public class RemoteLoader {
 	public static void main(String[] args) {
 		RemoteControl remoteControl = new RemoteControl();
 		
-		Light livingRoomLight = new Light("Living Room");
-		Light kitchenLight = new Light("Kitchen");
+		CeilingFan ceilingFan = new CeilingFan("Living Room");
 		
-		LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
-		LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+		CeilingFanMediumCommand ceilingFanMedium = new CeilingFanMediumCommand(ceilingFan);
+		CeilingFanHighCommand ceilingFanHigh = new CeilingFanHighCommand(ceilingFan);
+		CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
 		
-		LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
-		LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
-		
-		remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
-		remoteControl.setCommand(1, kitchenLightOn, kitchenLightOff);
+		remoteControl.setCommand(0, ceilingFanMedium, ceilingFanOff);
+		remoteControl.setCommand(1, ceilingFanHigh, ceilingFanOff);
 		
 		remoteControl.onButtonWasPushed(0);
-		remoteControl.onButtonWasPushed(1);
-		System.out.println(remoteControl);
-		
 		remoteControl.offButtonWasPushed(0);
-		remoteControl.offButtonWasPushed(1);
+		System.out.println(remoteControl);
+		remoteControl.undoButtonWasPushed();
+		
+		remoteControl.onButtonWasPushed(1);
 		System.out.println(remoteControl);
 		remoteControl.undoButtonWasPushed();
 	}
