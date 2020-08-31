1c07c3de7b5108fb966d27b955c0141ea425c094
ChoiTaesan <mijosan@naver.com>
2020-08-31 09:48:51 +0900

    Composite Pattern

---
+++ designpattern/src/designpattern/composite/Component.java
@@ -0,0 +1,19 @@
+package designpattern.composite;
+
+abstract public class Component {
+
+	private String name;
+	
+	public Component(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
