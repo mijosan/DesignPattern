e9b908c9e033be428b003b7f042a264344a94b53
ChoiTaesan <mijosan@naver.com>
2020-09-02 15:20:03 +0900

    Proxy Pattern

---
+++ designpattern/src/designpattern/proxy/Proxy.java
@@ -0,0 +1,21 @@
+package designpattern.proxy;
+
+public class Proxy implements Subject{
+	
+	private Subject real;
+	
+	public Proxy(Subject real) {
+		this.real = real;
+	}
+	
+	@Override
+	public void action1() {
+		System.out.println("간단한 업무 bt 프록시");
+	}
+	
+	@Override
+	public void action2() {
+		this.real.action2();
+	}
+	
+}
